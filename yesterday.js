11a188e5ceba370382c8b2a1773f8fa30f1f62b0
Victor Engmark <victor.engmark@gmail.com>
2011-08-30 17:07:01 +0200

    javascript: prefix is browser specific syntax

--- yesterday.js
+++ yesterday.js
@@ -1,4 +1,4 @@
-javascript:(function() {
+(function yesterday() {
     var date = new Date();
     date.setDate(date.getDate() - 1);
     window.location.href = 'https://bugzilla.mozilla.org/buglist.cgi?chfieldfrom=' + date.getFullYear() + '-' + ('0' + (date.getMonth()+1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
